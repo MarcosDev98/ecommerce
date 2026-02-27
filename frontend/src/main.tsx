5543afb4b77381a852a9b85ea025496ca245cf13
Marcos Aguado <marcos.aguado1998@gmail.com>
2026-02-27 12:11:46 -0300

    ci: add linting check

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+
+const variableInutil = 10; // Declara pero no uses
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
